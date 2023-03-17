0940c7a2d3e94ebe7a5412986cfc58677698d2a4
AbhiMishra1097 <abhimishra1097@gmail.com>
2023-03-17 12:00:34 +0530

    modifying app module to include components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { PracticeComponent } from './practice/practice.component';
+import { LoginUserService } from './services/login-user.service';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    PracticeComponent,
+    SortPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [LoginUserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
